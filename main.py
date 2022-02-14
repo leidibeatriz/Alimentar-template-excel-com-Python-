b8e9415692c64bd10d4852db931303ab315e4519
leidibeatriz <94936204+leidibeatriz@users.noreply.github.com>
2022-02-14 11:17:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from openpyxl import workbook, load_workbook
 
 
-
+#Criando a classe pessoa para preenchermos com dados
 class Pessoa:
     def __init__(self, nome, profissao, salariobruto,desconto):
         self.nome = nome
@@ -9,22 +9,22 @@ class Pessoa:
         self.salariobruto = salariobruto
         self.desconto=desconto
 
-    def calcula_salario(self):
+    def calcula_salario(self): #função para calcular o salário liquído
         return self.salariobruto - self.desconto
 
 
-dados = Pessoa('Clara Maria','Veterinária',1500,420)
+dados = Pessoa('Clara Maria','Veterinária',1500,420) #variável dados recebe valores para os atributos da classe Pessoa
 
-salario_liquido = dados.calcula_salario()
+salario_liquido = dados.calcula_salario() #variável salario+liquido recebe o resultado retornado pela função calcula_salario
 
 #abrindo arquivo excel
 
-template_excel = 'preenche.xlsx'
-wb = load_workbook(template_excel)
+template_excel = 'preenche.xlsx' #fazendo a variável receber o nome da planilha
+wb = load_workbook(template_excel) #carregando a planilha na variável wb
 
-ws = wb.active
+ws = wb.active #aqui pegamos a seção ativa da planilha, caso tenha mais de uma seção utilize wb['NomeSeção']
 
-#inserindo dados na planilha
+#inserindo dados na planilha da variavél dados
 ws['A3'] = dados.nome
 ws['B3'] = dados.professao
 ws['C3'] = dados.salariobruto
@@ -33,14 +33,14 @@ ws['D3'] = dados.desconto
 #salvando os dados na planilha
 wb.save(template_excel)
 
-#pegando valores
+#pegando valores de salário bruto e desconto ca Coluna C e D da Linha 3
 
-sb = ws['C3'].value
-desc = ws['D3'].value
+sb = ws['C3'].value #esse pega salário bruto
+desc = ws['D3'].value #esse pega desconto
 
-ws['E3'] = sb - desc
+ws['E3'] = sb - desc #aqui é calculado e inserido na planilha Coluna E Linha 3
 
-#salvando os dados na planilha
+#salvando os dados na planilha novamente
 wb.save(template_excel)
 
 
